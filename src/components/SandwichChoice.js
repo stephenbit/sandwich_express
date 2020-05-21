3a8e2f130c681e731d1bd3a140ab9c6c068c8d3a
Stephen Ramsay <rabrandom@gmail.com>
2020-05-21 22:01:28 +0100

    get photos displaying using map

--- src/components/SandwichChoice.js
+++ src/components/SandwichChoice.js
@@ -1,54 +1,65 @@
-import React from 'react';
-import EggMayo from "../images/EggMayo.jpeg";
-import CoronationChicken from "../images/CoronationChicken.jpeg";
-import Cheese from "../images/Cheese.jpeg";
+import React, { useState } from 'react';
 
 function SandwichChoice({sandwiches}) {
-
-// const [ a, setA ] = useState('')
-
-    function mapImageURLs(photoId) {
-      return "../images/" + photoId
-    }
-
-    const sandwichPhotoId = sandwiches.map(photoId => (
-      "../images/" + sandwiches.photoId
-    ))
-
-    const sandwichList = sandwiches.map(sandwiches => (
+    const sandwichList = sandwiches.map(sandwich => {
+      return (
         <ul>
-          <li key={sandwiches._links.sandwich}>
-            {sandwiches.name}
+          <li
+            key={sandwich._links.sandwich}>
             <img
-            src={sandwichPhotoId}
-            alt={sandwiches.name}
+            src={`images/${sandwich.photoId}.jpeg`}
+            alt={sandwich.name}
             />
+            {sandwich.name}
           </li>
         </ul>
-    ))
+    )
+    })
 
   return (
     <div>
       <h2>Choose Your Sandwich!</h2>
+
       {sandwichList}
-      <form>
+
+      {/* <form>
         <input type="image" src={EggMayo} alt="Egg Mayonaise Sandwich"/>
-        <label>Egg Mayonaise Sandwich</label><input type="number" id="quantity" name="quantity" min="1" max="10"/>
+        <label>Egg Mayonaise Sandwich</label>
+        <input type="number" id="quantity" name="quantity" min="1" max="10"/>
         <button>Add to basket</button>
       </form>
 
       <form>
         <input type="image" src={CoronationChicken} alt="Coronation Chicken Sandwich"/>
-        <label>Coronation Chicken Sandwich</label><input type="number" id="quantity" name="quantity" min="1" max="10"/>
+        <label>Coronation Chicken Sandwich</label>
+        <input type="number" id="quantity" name="quantity" min="1" max="10"/>
         <button>Add to basket</button>
       </form>
+
       <form>
         <input type="image" src={Cheese} alt="Cheese Sandwich"/>
-        <label>Cheese Sandwich</label><input type="number" id="quantity" name="quantity" min="1" max="10"/>
+        <label>Cheese Sandwich</label>
+        <input type="number" id="quantity" name="quantity" min="1" max="10"/>
         <button>Add to basket</button>
-      </form>
+      </form> */}
+
     </div>
+
+    
     );
+    const styles = StyleSheet.create({
+      container: {
+        marginTop: 50,
+      },
+      bigBlue: {
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 30,
+      },
+      red: {
+        color: 'red',
+      },
+    });
   };
 
 export default SandwichChoice;
